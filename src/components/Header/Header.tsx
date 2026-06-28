5b3c78f4e04989479ace21bb648ddeaadd7e3777
MyDuon <100708123+MyDuon@users.noreply.github.com>
2026-06-28 23:02:42 +0200

    made a hamburger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,19 @@
 import "./Header.css";
 import { Link } from "react-router";
 import resume from "../../assets/CV.pdf";
+import { useState } from "react";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
       <header className="header-container">
         <Link className="home-button" to="/">
           <p>My Hoang Duong</p>
         </Link>
-        <nav className="nav">
+
+        {/* Desktop navbar */}
+        <nav className={`nav ${menuOpen ? "open" : ""}`}>
           <a href="/">About</a>
           {/* <a href="/">Projects</a> */}
           <a href={resume} target="_blank">
@@ -19,6 +23,19 @@ function Header() {
             <p>Education</p>
           </Link>
         </nav>
+
+        {/* Hamburger button */}
+        <div
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+            console.log("menuOpen", menuOpen);
+          }}
+          className="hamburger-button"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </header>
     </>
   );
